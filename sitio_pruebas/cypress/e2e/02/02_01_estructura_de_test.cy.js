bd27b7f297c650fc33a01595ebe272500ce46d76
Manuel Mendoza <mmendoza@NTB-219.local>
2025-05-02 18:14:02 -0400

    test

--- sitio_pruebas/cypress/e2e/02/02_01_estructura_de_test.cy.js
+++ sitio_pruebas/cypress/e2e/02/02_01_estructura_de_test.cy.js
@@ -2,7 +2,7 @@ describe('Página principal', () => {
 
     it('Abre la versión en español', () => {
         cy.visit('http://localhost:5173/');
-        cy.url().should('include', 'localhossdt')
+        cy.url().should('include', 'localhost')
     })
 
 })
